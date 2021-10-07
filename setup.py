b64f0e33340c2b44b86e064025f88a593ddef347
IzumiSatoshi <izumisatoshi05@gmail.com>
2021-10-07 18:26:48 +0900

    setup.pyがrequirements.txtを参照するようにした

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import setuptools
 
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
 setuptools.setup(
     name='scraping_toolkit',
     version='1.0',
     author='IzumiSatoshi',
-    packages=setuptools.find_packages()
+    packages=_requires_from_file('requirements.txt')
 )
