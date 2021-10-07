160334ad657c13fdd2c0519090f7ba11963453f4
IzumiSatoshi <izumisatoshi05@gmail.com>
2021-10-07 19:00:11 +0900

    無事依存関係のインストールができるようになった

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
+"""
+参考
+https://deepblue-ts.co.jp/python/pypi-oss-package/
+"""
 import setuptools
+from os import path
+
+version = '1.1'
+package_name = "scraping_toolkit"
+root_dir = path.abspath(path.dirname(__file__))
+
+
+def _requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
 
 
 setuptools.setup(
     name='scraping_toolkit',
-    version='1.0',
+    version=version,
     author='IzumiSatoshi',
-    packages=setuptools.find_packages(where='scraping_toolkit')
+    install_requires=_requirements(),
+    packages=[package_name]
 )
