d9dab78fb303f4fafab717190e74ce15a56c69be
IzumiSatoshi <izumisatoshi05@gmail.com>
2021-10-07 18:31:15 +0900

    find_packagesにした

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 import setuptools
 
 
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
-
 setuptools.setup(
     name='scraping_toolkit',
     version='1.0',
     author='IzumiSatoshi',
-    packages=_requires_from_file('requirements.txt')
+    packages=setuptools.find_packages(where='scraping_toolkit')
 )
